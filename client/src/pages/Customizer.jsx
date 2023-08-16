774545bb43d9ec0285b06cd84b0d95ceaee4f928
sustainspicy <isrealspicy@gmail.com>
2023-08-16 11:29:39 +0700

    feature: openai API endpoint implemented

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -55,18 +55,18 @@ const Customizer = () => {
     if (!prompt) return alert("Please enter a prompt");
 
     try {
-      // setGeneratingImg(true);
-      // const response = await fetch('http://localhost:8080/api/v1/dalle', {
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'application/json'
-      //   },
-      //   body: JSON.stringify({
-      //     prompt,
-      //   })
-      // })
-      // const data = await response.json();
-      // handleDecals(type, `data:image/png;base64,${data.photo}`)
+      setGeneratingImg(true);
+      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          prompt,
+        }),
+      });
+      const data = await response.json();
+      handleDecals(type, `data:image/png;base64,${data.photo}`);
     } catch (error) {
       alert(error);
     } finally {

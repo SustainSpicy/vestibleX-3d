8e4d6cd5b902d978fd75604da270d1c2b38de754
sustainspicy <isrealspicy@gmail.com>
2023-08-16 10:13:06 +0700

    feature: canvas implemented

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -19,5 +19,37 @@ const Shirt = () => {
 
   const stateString = JSON.stringify(snap);
 
-  return <group key={stateString}></group>;
+  return (
+    <group key={stateString}>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      >
+        {snap.isFullTexture && (
+          <Decal
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+        {snap.isLogoTexture && (
+          <Decal
+            position={[0, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.15}
+            map={logoTexture}
+            // map-anisotropy={16}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
+      </mesh>
+    </group>
+  );
 };
+
+export default Shirt;

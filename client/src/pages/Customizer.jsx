b7b9b22d35a17403080de9223db0fbcdb2bee348
sustainspicy <isrealspicy@gmail.com>
2023-08-27 14:57:30 +0700

    feature: client build file generated

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -56,7 +56,7 @@ const Customizer = () => {
 
     try {
       setGeneratingImg(true);
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+      const response = await fetch(`${API_URL}/api/v1/dalle`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
